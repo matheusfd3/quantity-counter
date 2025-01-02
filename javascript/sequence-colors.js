8c83678a593a94577534e914040656192180b262
Matheus Ferreira <matheusfdantas03@gmail.com>
2025-01-01 23:34:31 -0300

    Update sequence color saturation for improved visibility in animations

--- javascript/sequence-colors.js
+++ javascript/sequence-colors.js
@@ -2,5 +2,5 @@ const maxSequence = 90;
 
 function getSequenceColor(sequence) {
     const hue = Math.max(0, 240 - (sequence / maxSequence) * 240);
-    return `hsl(${hue}, 60%, 50%)`;
+    return `hsl(${hue}, 70%, 50%)`;
 }
